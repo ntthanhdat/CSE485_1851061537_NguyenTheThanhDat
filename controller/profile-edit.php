5dd5e719a845e530dfa00d95ae853865e9b5702e
phamduong1671 <phamduong1671@users.noreply.github.com>
2021-01-18 20:37:31 +0700

    fix edit profile

--- controller/profile-edit.php
+++ controller/profile-edit.php
@@ -6,11 +6,8 @@ $fname=$_POST['fname'];
 $lname=$_POST['lname'];
 $phone=$_POST['phone'];
 $email=$_POST['email'];
-$add1=$_POST['address1'];
-$add2=$_POST['address2'];
+$add1=$_POST['address'];
 $city=$_POST['city'];
-$state=$_POST['state_country'];
-$zcode_pcode=$_POST['zcode_pcode'];
 $sql1 = "select * from users where email='$email'";
 $result = mysqli_query($conn, $sql1);
 if (mysqli_num_rows($result) > 0) {
@@ -19,7 +16,7 @@ if (mysqli_num_rows($result) > 0) {
 } else {
 
 $sql="update users set first_name='$fname',  last_name='$lname',  phone = '$phone', email ='$email' ,
-address1='$add1', address2='$add2', city='$city', zcode_pcode='$zcode_pcode'
+address1='$add1', city='$city'
 where userid=$id";
 
 mysqli_set_charset($conn,'UTF8');
